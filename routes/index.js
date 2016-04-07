cd801aee31a0b8d1e6cb7f5cd5f372114b7c92f3
quikstrike <quikstrik3@gmail.com>
2016-04-06 22:57:40 -0500

    Add auth endpoint to authorize with TwitchAlerts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var buildAuthUrl = require('./helpers/buildAuthUrl');
+var Promise = require('bluebird');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json('Hello World');
 });
+
+router.get('/auth',function(req, res, next){
+  var uri = buildAuthUrl();
+  res.redirect(uri)
+})
 
 module.exports = router;
