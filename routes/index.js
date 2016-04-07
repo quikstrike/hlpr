ec447891e59ace8473ff8f7ef89278cfa7359771
quikstrike <quikstrik3@gmail.com>
2016-04-07 00:14:05 -0500

    Add donations endpoint to grab donations from TwitchAlerts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var buildAuthUrl = require('./helpers/buildAuthUrl');
+var makeDonationsRequest = require('./helpers/makeDonationsRequest');
 var Promise = require('bluebird');
 var request = require('request-promise');
 
@@ -36,5 +37,18 @@ router.get('/auth', function (req, res, next) {
         res.redirect(uri)
     }
 });
+
+router.get('/donations', function(req, res, next) {
+    if(Object.keys(req.query).length > 0) {
+      Promise.resolve()
+      .bind(req.query)
+      .then(makeDonationsRequest)
+      .then(function(results) {
+        res.json(results)
+      })
+    } else {
+      res.status(400).json('No query parameters provided')
+    }
+})
 
 module.exports = router;
